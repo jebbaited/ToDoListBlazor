ca4a8515127b9e09be8427de938467158b63fa4e
Константин Драпак <kostadrapak@gmail.com>
2021-12-08 09:46:07 +0200

    Add project files.

---
+++ ToDoList/Models/TaskToDo.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Models
+{
+    public class TaskToDo
+    {
+        public string Task{ get; set; }
+        public DateTime AddedTime { get; set; }
+        public DateTime FinishAt { get; set; }
+    }
+}
